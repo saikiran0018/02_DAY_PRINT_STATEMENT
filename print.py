721b699e9a5548bcfe672294d90690f319d3d519
saikiran0018 <109685878+saikiran0018@users.noreply.github.com>
2022-07-21 18:24:32 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,33 @@
+# print('hello world')                      #hello world
+# print("python programming")               #python prigramming
+
+
+# print('python program'ming')              #syntax: invalid syntax
+# print("python program\"ming")             #python program"ming"         
+
+# print('''hello world'')                    #hello world
+# print('''python programming''')           #python programming
+
+
+# print("python programming \nlanguage")     #python programming
+#                                              language
+
+
+# print('python \
+# programming')                               #python programming
+
+
+# print("""Hello\
+#     world""")                               #hello    world
+
+
+# print("123")                                 #123
+# print("45.67")                               #45.67
+# print("""56.32""")                           #56.32
+
+# print(type(1))                                 #int
+# print(type(1.3))                               #float
+
+
+# print(type(''))                                #str
+# print(type(""))                                 #str
